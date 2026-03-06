59404552884cc09f7a5ce524b933ba72a86e1b4e
Bepis <coreyburdickwork@gmail.com>
2026-03-06 15:35:14 -0500

    added .gitignore

--- build/CMakeFiles/test.dir/src/balls.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/test.dir/src/balls.cpp.o: \
- /home/bepis/dev/physics_test/src/balls.cpp /usr/include/stdc-predef.h \
- /home/bepis/dev/physics_test/include/balls.h \
- /home/bepis/dev/physics_test/build/_deps/raylib-src/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/15/include/stdarg.h
--- build/CMakeFiles/test.dir/src/main.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/test.dir/src/main.cpp.o: \
- /home/bepis/dev/physics_test/src/main.cpp /usr/include/stdc-predef.h \
- /home/bepis/dev/physics_test/build/_deps/raylib-src/src/raylib.h \
- /usr/lib/gcc/x86_64-linux-gnu/15/include/stdarg.h \
- /home/bepis/dev/physics_test/include/game.h
